044748838ef2f8d07b19d06d2f854573ad2dbf9a
kimbaqueen <k.a.starr@live.com>
2022-07-05 20:55:19 -0600

    added structual HTML and styling

--- server/index.js
+++ server/index.js
@@ -10,6 +10,35 @@ const { seed } = require('./seed.js');
 app.use(express.json());
 app.use(cors());
 
+
+const bookGallery = document.getElementById("book-gallery");
+
+// ****need help with connecting to database & adding to website gallery***
+app.get()
+    // or fetch ()?
+    .then(req, res => {
+        // imageLoop.forEach (i => {
+        const image = document.createElement('img');
+        image.src = `${img_url} from database`;
+        image.alt = `Book image for ${title}`;
+        image.classList.add('galleryImg');
+        // });
+
+        image.addEventListener('mouseover', () => {
+            // hover text overlay - pulling title, author, description from database
+            // if this doesn't look right may switch to a popup instead
+        });
+
+        // image.addEventListener('mouseout', () => {
+        //     // goes back to normal
+        // });
+
+        bookGallery.appendChild(image);
+    });
+
+
+
+
 app.post('/seed', seed);
 
 app.listen(SERVER_PORT, () => console.log(`We listening up on ${SERVER_PORT}`));

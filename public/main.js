d67e21497483a658d3c60bac9a068575fc17c354
kimbaqueen <k.a.starr@live.com>
2022-07-11 17:29:01 -0600

    added some code

--- public/main.js
+++ public/main.js
@@ -1,13 +1,21 @@
 // const { forEach } = require("lodash");
 
+const { format } = require("sequelize/types/utils");
+
 
 const url = 'http://localhost:4003';
 
 const bookGallery = document.getElementById('book-gallery');
 const popup = document.getElementById('popup');
-const bookTitle = document.getElementById('book-title');
-const bookAuthor = document.getElementById('book-author');
-const bookDescription = document.getElementById('book-description');
+
+const bookForm = document.getElementById('book-form');
+
+// Do i need these below or is having them in my form event listener enough?
+let bookTitle = document.getElementById('book-title').value;
+let bookAuthor = document.getElementById('book-author').value;
+let bookImg = document.getElementById('book-img').value;
+let bookCategory = document.querySelector('#book-category').value;
+let bookDescription = document.getElementById('book-description').value;
 
 function getBooks() {
     console.log('get books test');
@@ -44,4 +52,25 @@ function displayBooks() {
 displayBooks();
 
 // front end filter by category button functions? 
+let filterInput = document.getElementById('filterButtons');
+filterInput.addEventListener('click', filterButtons)
+function filterButtons() {
+    // get value of book-category buttons when clicked and display filtered gallery
+    // do i need to create function for each button or is there a way to do it using DOM ID when clicked?
+};
+
 // sequelize select by category from table & display on front end?
+
+// Add.html - post form data to database
+format.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let bookTitle = document.getElementById('book-title').value;
+    let bookAuthor = document.getElementById('book-author').value;
+    let bookImg = document.getElementById('book-img').value;
+    let bookCategory = document.querySelector('#book-category').value;
+    let bookDescription = document.getElementById('book-description').value;
+
+    // how to get this front end to connect with backend and post values to my database?
+
+});
